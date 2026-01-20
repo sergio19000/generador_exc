02b2307b1228fa7e4b2dd6d1a3cb36893b25bb96
sergio19000 <119664418+sergio19000@users.noreply.github.com>
2026-01-20 09:26:58 +0000

    excuses gen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = [
+    'before the class',
+    'when I was sleeping',
+    'while I was exercising',
+    'during my lunch',
+    'while I was praying'
+  ];
+
+  function randomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  let excuse =
+    randomItem(who) + ' ' +
+    randomItem(action) + ' ' +
+    randomItem(what) + ' ' +
+    randomItem(when);
+
+  document.getElementById('excuse').innerHTML = excuse;
 };
+
