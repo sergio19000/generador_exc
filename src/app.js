78f766a719251fafc5df9ef99ef5aa591ccf6d65
sergio19000 <119664418+sergio19000@users.noreply.github.com>
2026-01-20 09:30:00 +0000

    change code

--- src/app.js
+++ src/app.js
@@ -9,13 +9,7 @@ window.onload = function() {
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
-  let when = [
-    'before the class',
-    'when I was sleeping',
-    'while I was exercising',
-    'during my lunch',
-    'while I was praying'
-  ];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
   function randomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
